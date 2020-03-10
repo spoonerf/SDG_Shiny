7cba431ef9f8b18d2281e6446a5ddf08f50ab950
Fiona Spooner <spoonerfiona@gmail.com>
2020-03-10 15:39:38 +0000

    have added another variable to look at the date of the most recent data available

--- app.R
+++ app.R
@@ -29,50 +29,59 @@ source("sdg_plotter.R")
 # Define UI for application that draws a histogram
 ui <- fluidPage(titlePanel(strong("SDG Indicators")),
                 
-                sidebarLayout(sidebarPanel(
-                    h3("Indicator Data Availability"),
-                    selectInput(
-                        "goal_view",
-                        h3("Select Goal:"),
-                        choices = list(
-                            "All Goals" = "All Goals",
-                            "Biosphere Pillar" = "the Biosphere Pillar",
-                            "Economy Pillar" = "the Economy Pillar",
-                            "Society Pillar" = "the Society Pillar",
-                            "Goal 1 - No Poverty" = "Goal 1",
-                            "Goal 2 - Zero Hunger" = "Goal 2",
-                            "Goal 3 - Good Health and wel-being" = "Goal 3",
-                            "Goal 4 - Quality Education" = "Goal 4",
-                            "Goal 5 - Gender Equality" = "Goal 5",
-                            "Goal 6 - Clean Water and Sanitation" = "Goal 6",
-                            "Goal 7 - Affordable and Clean Energy" = "Goal 7",
-                            "Goal 8 - Decent Work and Economic Growth" = "Goal 8",
-                            "Goal 9 - Industry, Innovation and Infrastructure" = "Goal 9",
-                            "Goal 10 - Reduced Inequality" = "Goal 10",
-                            "Goal 11 - Sustainable Cities and Communities" = "Goal 11",
-                            "Goal 12 -Responsible Consumption and Production" = "Goal 12",
-                            "Goal 13 - Climate Action" = "Goal 13",
-                            "Goal 14 - Life Below Water" = "Goal 14",
-                            "Goal 15 - Life on Land" = "Goal 15",
-                            "Goal 16 - Peace, Justice and Strong Institutions" = "Goal 16",
-                            "Goal 17 - Partnerships to achieve the Goal" = "Goal 17"
-                            
+                sidebarLayout(
+                    sidebarPanel(
+                        h4("Indicator Data Availability"),
+                        selectInput(
+                            "goal_view",
+                            h3("Select Goal:"),
+                            choices = list(
+                                "All Goals" = "All Goals",
+                                "Biosphere Pillar" = "the Biosphere Pillar",
+                                "Economy Pillar" = "the Economy Pillar",
+                                "Society Pillar" = "the Society Pillar",
+                                "Goal 1 - No Poverty" = "Goal 1",
+                                "Goal 2 - Zero Hunger" = "Goal 2",
+                                "Goal 3 - Good Health and wel-being" = "Goal 3",
+                                "Goal 4 - Quality Education" = "Goal 4",
+                                "Goal 5 - Gender Equality" = "Goal 5",
+                                "Goal 6 - Clean Water and Sanitation" = "Goal 6",
+                                "Goal 7 - Affordable and Clean Energy" = "Goal 7",
+                                "Goal 8 - Decent Work and Economic Growth" = "Goal 8",
+                                "Goal 9 - Industry, Innovation and Infrastructure" = "Goal 9",
+                                "Goal 10 - Reduced Inequality" = "Goal 10",
+                                "Goal 11 - Sustainable Cities and Communities" = "Goal 11",
+                                "Goal 12 - Responsible Consumption and Production" = "Goal 12",
+                                "Goal 13 - Climate Action" = "Goal 13",
+                                "Goal 14 - Life Below Water" = "Goal 14",
+                                "Goal 15 - Life on Land" = "Goal 15",
+                                "Goal 16 - Peace, Justice and Strong Institutions" = "Goal 16",
+                                "Goal 17 - Partnerships to achieve the Goal" = "Goal 17"
+                                
+                            ),
+                            selected = "Goal 14"
                         ),
-                        selected = "Goal 14"
-                    )
-                ),
-                
-                mainPanel(leafletOutput("map", width = 800, height = 500))
-                )
-                )
+                        
+                        selectInput(
+                            "var",
+                            label = h4("Choose a variable to display"),
+                            choices = c(
+                                "Number of Indicators with Data" = "num_indicators",
+                                "Mean Age of Most Recent Indicator Data" = "mean_age"
+                            ),
+                            selected = "num_indicators"
+                        )
+                    ),
+                    
+                    mainPanel(leafletOutput(
+                        "map", width = 800, height = 500
+                    ))
+                ))
 
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-    output$selected_var <- renderText({
-        paste("You have selected", input$goal_view)
-    })
-    
+      
     output$map <- renderLeaflet({
         goal_in <- switch(
             input$goal_view,
@@ -99,7 +108,9 @@ server <- function(input, output) {
             "Goal 17" = 17
         )
         
-        sdg_plotter(goal_view = goal_in)
+        var <- input$var
+        
+        sdg_plotter(goal_view = goal_in, var_in = var)
     })
     
 }
--- sdg_plotter.R
+++ sdg_plotter.R
@@ -18,8 +18,9 @@ world_sf <- world %>%
   dplyr::mutate(code = iso_a3, region_un = as.factor(region_un)) %>%
   left_join(., glob_inds, by = "code")
 
-sdg_plotter <- function(goal_view = 1:17){
+sdg_plotter <- function(goal_view = 1:17, var_in = "num_indicators"){
   
+  if(var_in == "num_indicators"){
   num_ind_countries <- world_sf %>%
     dplyr::filter(goal %in% {{goal_view}} &
              region_un != "Antarctica" &
@@ -31,27 +32,58 @@ sdg_plotter <- function(goal_view = 1:17){
     dplyr::mutate(total_inds = n_distinct(indicator)) %>% 
     dplyr::group_by(entity) %>%
     dplyr::summarise(sum_ind = sum(!is.na(latest_data_year)), prop_ind = 100*(sum_ind/unique(total_inds))) %>% 
-    mutate(prop_ind = replace(prop_ind, prop_ind == 0, NA))
-  
-  num_ind_countries <- sf::as_Spatial(num_ind_countries)
-  
+    mutate(disp_val = replace(prop_ind, prop_ind == 0, NA))
+
   bins <- c(1,seq(20,100, 20))
-  
-  pal <-
-    leaflet::colorBin("YlOrRd", domain = num_ind_countries$prop_ind, bins = bins)
-  
+ 
   labels <- sprintf(
     "<strong>%s</strong><br/>%g indicators",
     num_ind_countries$entity,
     num_ind_countries$sum_ind
   ) %>% lapply(htmltools::HTML)
   
-  leaflet::leaflet(num_ind_countries) %>%
+   map_out <- sf::as_Spatial(num_ind_countries)
+  
+   }
+  
+ 
+  if(var_in == "mean_age"){
+    
+    mean_yr_countries <- world_sf %>%
+      dplyr::filter(goal %in% {{goal_view}} &
+                      region_un != "Antarctica" &
+                      region_un != "Seven seas (open ocean)") %>% 
+      group_by(entity, indicator) %>%
+      filter(latest_data_year == max(latest_data_year)) %>%
+      ungroup() %>%
+      group_by(entity) %>%
+      summarise(mean_year = mean(latest_data_year), disp_val = mean_year)
+    
+    if (min(mean_yr_countries$disp_val) < 2014){
+      bins <- round(seq(min(mean_yr_countries$disp_val),2020, length.out = 6))
+    }else{
+      bins <- seq(min(mean_yr_countries$disp_val),2020)
+    }
+    
+    labels <- sprintf(
+      "<strong>%s</strong><br/>Average year of data availability: %g",
+      mean_yr_countries$entity,
+      mean_yr_countries$disp_val
+    ) %>% lapply(htmltools::HTML)
+    
+    map_out <- sf::as_Spatial(mean_yr_countries)
+  }
+  
+  
+   pal <-
+    leaflet::colorBin("YlOrRd", domain = map_out$disp_val, bins = bins)
+  
+  leaflet::leaflet(map_out) %>%
     #clearBounds() %>% 
     setView(0, 30, 2) %>%
     addProviderTiles("CartoDB.Positron") %>%
     addPolygons(
-      fillColor = ~ pal(prop_ind),
+      fillColor = ~ pal(disp_val),
       weight = 2,
       opacity = 1,
       color = "white",
@@ -73,10 +105,11 @@ sdg_plotter <- function(goal_view = 1:17){
     ) %>%
     addLegend(
       pal = pal,
-      values = ~ prop_ind,
+      values = ~ disp_val,
       opacity = 0.7,
-      title = "% Indicators\n with Data",
-      position = "bottomleft"
+      title = ifelse(var_in == "num_indicators","% Indicators\n with Data", "Date of Most Recent Data"),   #should change to switch
+      position = "bottomleft",
+      labFormat = labelFormat(big.mark = "")
     )
   
   
